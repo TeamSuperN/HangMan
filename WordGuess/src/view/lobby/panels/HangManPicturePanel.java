70a39b18a2916e6df82d2d4e9cabebd3ce452058
Daniel Thomas <daniel.l.thomas-1@ou.edu>
2020-04-21 16:24:18 -0500

    Generalized path to hangman image

--- WordGuess/src/view/lobby/panels/HangManPicturePanel.java
+++ WordGuess/src/view/lobby/panels/HangManPicturePanel.java
@@ -1,6 +1,8 @@
 package view.lobby.panels;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -8,12 +10,21 @@ import javax.swing.JPanel;
 
 public class HangManPicturePanel extends JPanel
 {
+	JLabel hangManLabel;
+	
 	public HangManPicturePanel() throws IOException
 	{
-		String hangManImagePath = "C://Users/Public/Documents/HangManPicture.jpg";
+		String imageFileName = "HangManPicture.jpg";
+		
+		Path relativeCurrentPath = Paths.get("");
+		String absoluteCurrentPath = relativeCurrentPath.toAbsolutePath().toString();
+		String imagePath = absoluteCurrentPath + "/images/" + imageFileName;
+		//System.out.println(absolutePath);
+		
+		//String hangManImagePath = "C://Users/Public/Documents/HangManPicture.jpg";
 		
 		JLabel hangManLabel = new JLabel();
-		hangManLabel.setIcon(new ImageIcon(hangManImagePath));// your image here
+		hangManLabel.setIcon(new ImageIcon(imagePath));// your image here
 	
 		
         add(hangManLabel);
