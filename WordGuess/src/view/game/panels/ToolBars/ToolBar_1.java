6d99b0dd68482df1a701b44ca98fa7e271db876b
cfick_9v0yl88 <cfick_9v0yl88@DESKTOP-APUOSP2>
2020-04-29 13:25:09 -0500

    test again
    Signed-off-by: cfick_9v0yl88 <cfick_9v0yl88@DESKTOP-APUOSP2>

--- WordGuess/src/view/game/panels/ToolBars/ToolBar_1.java
+++ WordGuess/src/view/game/panels/ToolBars/ToolBar_1.java
@@ -6,9 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JToolBar;
 
 import controller.Actions;
